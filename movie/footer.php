861b66c56d417da569957c6f0f576596f5b6e747
ZhangMouQWQ <106049042+ZhangMouQWQ@users.noreply.github.com>
2022-05-28 17:52:04 +0800

    Add files via upload

---
+++ movie/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+<body>
+    <footer class="footer bg-dark p-3 text-light text-center">
+        <p class="m-1">
+            <a class="text-decoration-none text-light" href=""><span> 河南工业职业技术学院 </span></a>
+            <span>|</span>
+            <a class="text-decoration-none text-light" href=""><span> 电子信息工程学院 </span></a>
+            <span>|</span>
+            <a class="text-decoration-none text-light" href=""><span> 计算机应用技术2005班 </span></a>
+        </p>
+    </footer>
+</body>
+</html>
